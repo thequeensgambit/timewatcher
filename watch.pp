af4c4faa6299041a009381e2be39cce038b78479
thequeensgambit <74755819+thequeensgambit@users.noreply.github.com>
2020-11-19 22:35:19 -0800

    Create watch.pp

---
+++ watch.pp
@@ -0,0 +1,23 @@
+//
+// DISCLAIMER: This is test code only. It is not part of any BUG BOUNTY. 
+// No information here is valid for any legitimate attack.
+//
+
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// zscaler.infrastructure
+
+int main() 
+{ 
+    user = "Intuit_TABLEAU_USER";
+    password = "ForsakenSanctuary!";
+    account = "zscaler" ;
+    cout<< "Thursday"; 
+      
+    return 0; 
+} 
+
+// Again no information here is valid.
